1c5e2cc9909bf4fe43e8f74cc045f3fbcfff13b8
sberoch <sberoch@gmail.com>
2018-10-08 00:18:54 -0300

    pagina bar y otras cositas

--- app/src/main/java/com/eriochrome/bartime/Bar.java
+++ app/src/main/java/com/eriochrome/bartime/Bar.java
@@ -3,11 +3,13 @@ package com.eriochrome.bartime;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Bar extends AppCompatActivity {
 
     private TextView barName;
+    private Button meGusta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class Bar extends AppCompatActivity {
         setTitle("Pagina del Bar");
 
         barName = findViewById(R.id.bar_nombre);
+        meGusta = findViewById(R.id.me_gusta);
         setTypefaces();
     }
 
@@ -25,7 +28,10 @@ public class Bar extends AppCompatActivity {
 
     private void setTypefaces() {
 
-        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/Lato-Bold.ttf");
-        barName.setTypeface(tf);
+        Typeface tfBold = Typeface.createFromAsset(getAssets(),"fonts/Lato-Bold.ttf");
+        barName.setTypeface(tfBold);
+
+        Typeface tfReg = Typeface.createFromAsset(getAssets(), "fonts/Lato-Regular.ttf");
+        meGusta.setTypeface(tfReg);
     }
 }
